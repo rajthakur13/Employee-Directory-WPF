e3e6edaacfeef2889695c6f3b06a71e43912a2a6
Raj Thakur <raj.t@pravaltech.com>
2025-02-06 18:22:39 +0530

    Implemented pagination functionality

--- EmployeeDirectory/ViewModels/EmployeeViewModel.cs
+++ EmployeeDirectory/ViewModels/EmployeeViewModel.cs
@@ -14,6 +14,7 @@ namespace EmployeeDirectory.WPF.ViewModels
         private readonly IWindowManager _windowManager;
         private readonly EmployeeService _employeeService;
         private Employee _selectedEmployee;
+        private List<Employee> _allEmployees = new List<Employee>();
         public BindableCollection<Employee> Employees { get; set; }
         public Employee SelectedEmployee
         {
@@ -29,21 +30,150 @@ namespace EmployeeDirectory.WPF.ViewModels
 
             }
         }
-        protected override void OnViewLoaded(object view)
+        private int _currentPage = 1;
+        //private int _itemsPerPage = 10;
+        private int _totalPages;
+
+        public List<int> ItemsPerPageOptions { get; set; } = new List<int> { 5, 10, 20, 50 };
+
+        private int _itemsPerPage;
+        public int ItemsPerPage
         {
-            base.OnViewLoaded(view);
-            RefreshEmployees();
-            //Employees.Add(new Employee { Id = 1, Name = "Jim Carter", Position = "Software Engineer", Email = "jim.c@pravaltech.com", Department = "IT" });
-            //Employees.Add(new Employee { Id = 2, Name = "Alex Murphy", Position = "Product Manager", Email = "alex.m@pravaltech.com", Department = "Buisness" });
-            //Employees.Add(new Employee { Id = 3, Name = "Megan Bowen", Position = "Designer", Email = "megan.bown@pravaltech.com", Department = "Branding" });
-            //Employees.Add(new Employee { Id = 4, Name = "Zack Nestor", Position = "Senior Software Engineer", Email = "zack.w@pravaltech.com", Department = "IT" });
-            //Employees.Add(new Employee { Id = 5, Name = "Nester Wike", Position = "QA Specialist", Email = "nester.wi@pravaltech.com", Department = "Quality Assurance" });
+            get => _itemsPerPage;
+            set
+            {
+                if (_itemsPerPage != value)
+                {
+                    _itemsPerPage = value;
+                    NotifyOfPropertyChange(() => ItemsPerPage);
+                    UpdatePagination();
+                }
+            }
         }
+
+        public int CurrentPage
+        {
+            get => _currentPage;
+            set
+            {
+                _currentPage = value;
+                NotifyOfPropertyChange(() => CurrentPage);
+                NotifyOfPropertyChange(() => PageInfo);
+                LoadEmployees();
+            }
+        }
+
+        public int TotalPages
+        {
+            get => _totalPages;
+            set
+            {
+                _totalPages = value;
+                NotifyOfPropertyChange(() => TotalPages);
+                NotifyOfPropertyChange(() => PageInfo);
+            }
+        }
+
+        public string PageInfo => $"Page {CurrentPage} of {TotalPages}";
+
+        public bool CanGoToFirstPage => CurrentPage > 1;
+        public bool CanGoToPreviousPage => CurrentPage > 1;
+        public bool CanGoToNextPage => CurrentPage < TotalPages;
+        public bool CanGoToLastPage => CurrentPage < TotalPages;
+
+        public void GoToFirstPage()
+        {
+            if (CanGoToFirstPage)
+            {
+                CurrentPage = 1;
+                LoadEmployees();
+                NotifyOfPropertyChange(() => CanGoToFirstPage);
+                NotifyOfPropertyChange(() => CanGoToPreviousPage);
+                NotifyOfPropertyChange(() => CanGoToNextPage);
+                NotifyOfPropertyChange(() => CanGoToLastPage);
+            }
+        }
+
+        public void GoToPreviousPage()
+        {
+            if (CanGoToPreviousPage)
+            {
+                CurrentPage--;
+                LoadEmployees();
+                NotifyOfPropertyChange(() => CanGoToFirstPage);
+                NotifyOfPropertyChange(() => CanGoToPreviousPage);
+                NotifyOfPropertyChange(() => CanGoToNextPage);
+                NotifyOfPropertyChange(() => CanGoToLastPage);
+            }
+        }
+
+        public void GoToNextPage()
+        {
+            if (CanGoToNextPage)
+            {
+                CurrentPage++;
+                LoadEmployees();
+                NotifyOfPropertyChange(() => CanGoToFirstPage);
+                NotifyOfPropertyChange(() => CanGoToPreviousPage);
+                NotifyOfPropertyChange(() => CanGoToNextPage);
+                NotifyOfPropertyChange(() => CanGoToLastPage);
+            }
+        }
+
+        public void GoToLastPage()
+        {
+            if (CanGoToLastPage)
+            {
+                CurrentPage = TotalPages;
+                LoadEmployees();
+                NotifyOfPropertyChange(() => CanGoToFirstPage);
+                NotifyOfPropertyChange(() => CanGoToPreviousPage);
+                NotifyOfPropertyChange(() => CanGoToNextPage);
+                NotifyOfPropertyChange(() => CanGoToLastPage);
+            }
+        }
+
         public EmployeeViewModel(IWindowManager windowManager, EmployeeService employeeService)
         {
             _windowManager = windowManager;
             _employeeService = employeeService;
             Employees = new BindableCollection<Employee>();
+            ItemsPerPage = 10;
+        }
+
+        protected override void OnViewLoaded(object view)
+        {
+            base.OnViewLoaded(view);
+            _allEmployees = _employeeService.GetAllEmployees().ToList();
+            UpdatePagination();
+        }
+
+        private void UpdatePagination()
+        {
+            if (ItemsPerPage <= 0) ItemsPerPage = 10;
+            TotalPages = (int)Math.Ceiling((double)_allEmployees.Count / ItemsPerPage);
+            CurrentPage = 1;
+            LoadEmployees();
+            NotifyOfPropertyChange(() => CanGoToFirstPage);
+            NotifyOfPropertyChange(() => CanGoToPreviousPage);
+            NotifyOfPropertyChange(() => CanGoToNextPage);
+            NotifyOfPropertyChange(() => CanGoToLastPage);
+        }
+
+        private void LoadEmployees()
+        {
+            Employees.Clear();
+            var pagedEmployees = _allEmployees
+                .Skip((CurrentPage - 1) * ItemsPerPage)
+                .Take(ItemsPerPage)
+                .ToList();
+
+            Employees.AddRange(pagedEmployees);
+            NotifyOfPropertyChange(() => CanGoToFirstPage);
+            NotifyOfPropertyChange(() => CanGoToPreviousPage);
+            NotifyOfPropertyChange(() => CanGoToNextPage);
+            NotifyOfPropertyChange(() => CanGoToLastPage);
+            NotifyOfPropertyChange(() => PageInfo);
         }
 
         public void AddEmployee()
@@ -54,12 +184,14 @@ namespace EmployeeDirectory.WPF.ViewModels
         }
         private void RefreshEmployees()
         {
-            var employeeList = _employeeService.GetAllEmployees();
-            Employees.Clear();
-            foreach (var employee in employeeList)
-            {
-                Employees.Add(employee);
-            }
+            //var employeeList = _employeeService.GetAllEmployees();
+            //Employees.Clear();
+            //foreach (var employee in employeeList)
+            //{
+            //    Employees.Add(employee);
+            //}
+            _allEmployees = _employeeService.GetAllEmployees().ToList();
+            UpdatePagination();
         }
         public void DeleteEmployee()
         {
